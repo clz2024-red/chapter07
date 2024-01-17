a6e2efff51af884eeedd34e990c1e01c14c8233b
clz2024.red <clz2024.red@gmail.com>
2024-01-17 16:12:43 +0900

    thread echo

--- src/echo/Sever.java
+++ src/echo/Sever.java
@@ -24,39 +24,21 @@ public class Sever {
 		//서버시작
 		System.out.println("<서버시작>");
 		System.out.println("==================================================");
-		System.out.println("[연결을 기다리고 있습니다.]");
 		
-		//클라이언트가 접속을 하면 accept()가 실행됨
-		Socket socket = serverSocket.accept();
-		System.out.println("[클라이언트가 연결 되었습니다.]");
-		
-		
-		//메세지 받기용 스트림
-		InputStream is = socket.getInputStream();
-		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-		BufferedReader br = new BufferedReader(isr);
-		
-		//메세지 보내기용 스트림
-		OutputStream os = socket.getOutputStream();
-		OutputStreamWriter isw = new OutputStreamWriter(os, "UTF-8");
-		BufferedWriter bw = new BufferedWriter(isw);
-		
-		//////
+		//반복
 		while(true) {
-			//메세지 받기
-			String msg = br.readLine();
-			if(msg==null) {
-				break;
-			}
+			System.out.println("[연결을 기다리고 있습니다.]");
 			
-			System.out.println("받은메세지:" + msg);
+			//클라이언트가 접속을 하면 accept()가 실행됨
+			Socket socket = serverSocket.accept();
 			
-			//메세지 보내기
-			bw.write(msg);
-			bw.newLine();
-			bw.flush();
+			//출장   thread.start()
+			Thread thread = new ServerThread(socket);
+			thread.start();
+			
 		}
-		
+	
+		/*
 		System.out.println("=======================================");
 		System.out.println("<서버 종료>");
 		
@@ -65,7 +47,7 @@ public class Sever {
 		br.close();
 		socket.close();
 		serverSocket.close();
-
+		*/
 	}
 
 }
